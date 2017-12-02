0c762d0a5222b8250b42dfd8e5db5eb61280d05d
ccocot <ccocot@bc0de.net>
2017-12-03 03:09:14 +0700

    Update inquirer

--- unfollownotfollowback.js
+++ unfollownotfollowback.js
@@ -7,66 +7,84 @@
 
 const Client = require('instagram-private-api').V1;
 const delay = require('delay');
+const chalk = require('chalk');
 const _ = require('lodash');
+const inquirer = require('inquirer');
 
-const User = {
-    username: '', // Your Username Instagram
-    password: '', // Your Password Instagram
-}
+const User = [
+	{
+		type:'input',
+		name:'username',
+		message:'Insert Username'
+	},
+	{
+		type:'password',
+		name:'password',
+		message:'Insert Password',
+		mask:'*'
+	},
+	{
+		type:'input',
+		name:'sleep',
+		message:'Insert Sleep (In MiliSeconds)',
+		validate: function(value){
+			value = value.match(/[0-9]/);
+			if (value) return true;
+			return 'Delay is number';
+		}
+	}
+]
 
-const doLogin = async function(User){
+const Login = async function(User){
 
-    /** Save Account **/
     const Device = new Client.Device(User.username);
     const Storage = new Client.CookieMemoryStorage();
     const session = new Client.Session(Device, Storage);
 
     try {
-        await Client.Session.create(Device, Storage, User.username, User.password);
-        const accountId = await session.getAccountId();
-        return Promise.resolve({session,accountId});
+        await Client.Session.create(Device, Storage, User.username, User.password)
+        const account = await session.getAccount();
+        return Promise.resolve({session,account});
     } catch (err) {
         return Promise.reject(err);
     }
 
 }
 
-const getFollowers = async function(session, accountId, cursor){
-
-	const feed = new Client.Feed.AccountFollowers(session, accountId);
-	if (cursor) {
-		feed.setCursor(cursor);
-	}
-	try {
-		const result = await feed.get();
-		Promise.all(result.map(async(account) => {
-			AccountFollowers.push(account.params.id);
-		}))
-		if (feed.isMoreAvailable()) {
-			const getCursor = feed.getCursor();
-			await getFollowers(session, accountId, getCursor);
-		}
-	} catch (err) {
+const Followers = async function(session, id){
+	const feed = new Client.Feed.AccountFollowers(session, id);
+	try{
+		const Pollowers = [];
+		var cursor;
+		do {
+			if (cursor) feed.setCursor(cursor);
+			const getPollowers = await feed.get();
+			await Promise.all(getPollowers.map(async(akun) => {
+				Pollowers.push(akun.id);
+			}))
+			cursor = await feed.getCursor();
+		} while(feed.isMoreAvailable());
+		return Promise.resolve(Pollowers);
+	} catch(err){
 		return Promise.reject(err);
 	}
 }
 
-const getFollowing = async function(session, accountId, cursor){
-
-	const feed = new Client.Feed.AccountFollowing(session, accountId);
-	if (cursor) {
-		feed.setCursor(cursor);
-	}
-	try {
-		const result = await feed.get();
-		Promise.all(result.map(async(account) => {
-			AccountFollowing.push(account.params.id);
-		}))
-		if (feed.isMoreAvailable()) {
-			const getCursor = feed.getCursor();
-			await getFollowing(session, accountId, getCursor);
-		}
-	} catch (err) {
+const Following = async function(session, id){
+	const feed = new Client.Feed.AccountFollowing(session, id);
+	try{
+		const Pollowing = [];
+		var cursor;
+		do {
+			if (cursor) feed.setCursor(cursor);
+			const getPollowers = await feed.get();
+			await Promise.all(getPollowers.map(async(akun) => {
+				Pollowing.push(akun.id);
+			}))
+			cursor = await feed.getCursor();
+		} while(feed.isMoreAvailable());
+		return Promise.resolve(Pollowing);
+	} catch(err){
 		return Promise.reject(err);
 	}
 }
@@ -74,44 +92,54 @@ const getFollowing = async function(session, accountId, cursor){
 const Unfollow = async function(session, accountId){
 	try {
 		await Client.Relationship.destroy(session, accountId);
-		console.log("Unfollow %s => SUKSES", accountId);
+		return chalk`{bold.green SUKSES}`;
 	} catch (err){
-		console.log("Unfollow %s => GAGAL", accountId);
+		return chalk`{bold.red GAGAL}`;
 	}
 }
 
-const Excute = async function(User){
-	
+const Excute = async function(User,sleep){
 	try {
-
-		const data = await doLogin(User);
-		const Followers = getFollowers(data.session,data.accountId);
-		const Following = getFollowing(data.session,data.accountId);
-		await Promise.all([Followers, Following]);
-		await Promise.all(AccountFollowing.map(async(account) => {
-			if (!AccountFollowers.includes(account)) {
+		console.log(chalk`\n{yellow [?] Try to Login ....}`);
+		const doLogin = await Login(User);
+		console.log(chalk`{green [+] Login Succsess}, {yellow Try to get Followers and Following ....}`);
+		const task = [
+			Followers(doLogin.session, doLogin.account.id),
+			Following(doLogin.session, doLogin.account.id)
+		]
+		const [getFollowers, getFollowing] = await Promise.all(task);
+		console.log(chalk`{blue  | Followers : ${getFollowers.length}\n | Following : ${getFollowing.length}}`);
+		var AccountToUnfollow = [];
+		await Promise.all(getFollowing.map(async(account) => {
+			if (!getFollowers.includes(account)) {
 				await AccountToUnfollow.push(account);
 			}
 		}));
-		console.log("Account Followers : %s", AccountFollowers.length);
-		console.log("Account Following : %s", AccountFollowing.length);
-		console.log("Account To Unfollow: %s", AccountToUnfollow.length);
-		console.log("Unfollow all instagram users that are not following back\n")
-		AccountToUnfollow = _.chunk(AccountToUnfollow, 5);
+		console.log(chalk`{blue  | Account To Unfollow : ${AccountToUnfollow.length}}`)
+		AccountToUnfollow = _.chunk(AccountToUnfollow, 10);
 		for (let i = 0; i < AccountToUnfollow.length; i++) {
-			await Promise.all(AccountToUnfollow[i].map(async(id) => {
-				await Unfollow(data.session,id);				
+			await Promise.all(AccountToUnfollow[i].map(async(akun) => {
+				const doUnfollow = await Unfollow(doLogin.session, akun);
+				console.log(chalk`Unfollow {blue [${akun}]} => ${doUnfollow}`);				
 			}))
-			await console.log('[-] Delay For 30s');
-			await delay(30000);	
+			await console.log(chalk`{yellow [-] Delay For ${sleep} MiliSeconds}`);
+			await delay(sleep);	
 		}
-	} catch(err){
+	} catch(err) {
 		console.log(err);
 	}
 }
 
-const AccountFollowers = [];
-const AccountFollowing = [];
-var AccountToUnfollow = [];
+console.log(chalk`
+{bold Instagram Unfollow not Followback Tools Media}
+{green BC0DE.NET - NAONLAH.NET - WingKocoli}
+{bold.red Code By Ccocot | ccocot@bc0de.net}
+`);
 
-Excute(User);
+inquirer.prompt(User)
+	.then(answers => {
+		Excute({
+			username:answers.username,
+			password:answers.password
+		},answers.sleep);
+	})
